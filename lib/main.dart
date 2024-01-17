f60160f7d6f01ad61204b8ad321de9b214792592
Naoya-Otani <naoyaotani914@gmail.com>
2024-01-17 17:53:46 +0900

    style change

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List items = [];
+  final TextEditingController _todoController = TextEditingController();
 
-  Future<void> getData() async {
+  Future<void> getData(String keyword) async {
+    // 現状はAPIキーをハードコーディングしている
     var response =
         await http.get(Uri.https('www.googleapis.com', '/books/v1/volumes', {
-      'q': '{Next.js}',
+      'q': '{${keyword}}',
       'maxResults': '40',
       'langRestrict': 'ja',
       'key': 'AIzaSyBlWk_MJ73tQYkJMlDK8rixl9AgebBl3O0'
@@ -46,37 +48,58 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // @override
-  // void initState() {
-  //   super.initState();
+  @override
+  void initState() {
+    super.initState();
 
-  //   getData();
-  // }
+    getData("TypeScript");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Google Books API'),
-      ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Column(
-              children: <Widget>[
-                ListTile(
-                  leading: Image.network(
-                    items[index]['volumeInfo']['imageLinks']['thumbnail'],
-                  ),
-                  title: Text(items[index]['volumeInfo']['title']),
-                  subtitle: Text(items[index]['volumeInfo']['publishedDate']),
-                ),
-              ],
+        appBar: AppBar(
+          title: const Text('Google Books API'),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: _todoController,
+              decoration: const InputDecoration(
+                hintText: '本を検索...',
+                contentPadding: EdgeInsets.all(20),
+              ),
             ),
-          );
-        },
-      ),
-    );
+            ElevatedButton(
+              onPressed: () {
+                print("pressed");
+              },
+              child: Container(
+                  alignment: Alignment.center, child: const Text('検索')),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Image.network(
+                            items[index]['volumeInfo']['imageLinks']
+                                ['thumbnail'],
+                          ),
+                          title: Text(items[index]['volumeInfo']['title']),
+                          subtitle:
+                              Text(items[index]['volumeInfo']['publishedDate']),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ));
   }
 }
